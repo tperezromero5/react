9762dbdac38f8f04c45660c82845f60ad307354b
tperezromero5 <tomasperezromero5@gmail.com>
2023-10-31 21:01:58 -0300

    clase 3 de react

---
+++ src/App.js
@@ -0,0 +1,3 @@
+function App(){
+    return <h1>hola desde app</h1>}
+    export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
+import App from "./App";
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
-function App(){
-    return <h1>comeme la pija</h1>
-}
+/*function App(){
+    return <h1>hola que tal buen dia</h1> <input style={{color:"red", fontSize:"20px"}}/>*/
+
 root.render(<App/>)
